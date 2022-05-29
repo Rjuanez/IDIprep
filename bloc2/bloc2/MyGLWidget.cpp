4be0feac2304bacb8b26a00e68521a38cdf294d8
Ruben <ruben@netvn-1.home>
2022-05-30 00:35:19 +0200

    .

--- bloc2/bloc2/MyGLWidget.cpp
+++ bloc2/bloc2/MyGLWidget.cpp
@@ -100,9 +100,10 @@ void MyGLWidget::viewTransform ()
 {
 	//glm::mat4 View = glm::lookAt (OBS, VRP, UP);
     glm::mat4 View (1.0f);
-    View = glm::rotate(View, angleY, glm::vec3(0,0,1));
-    View = glm::rotate(View, angleX, glm::vec3(1,0,0));
     View = glm::translate(View, glm::vec3(0, 0, -radiEscena*1.5));
+    View = glm::rotate(View, angleX, glm::vec3(1,0,0));
+    View = glm::rotate(View, angleY, glm::vec3(0,1,0));
+    View = glm::translate(View, -VRP);
 	glUniformMatrix4fv (viewLoc, 1, GL_FALSE, &View[0][0]);
 }
 
